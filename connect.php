a70aed95832ee5a2b6067d7c05f8cd2d7df1f694
Shaiful Islam <mug.shaiful97@gmail.com>
2022-10-20 16:03:16 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,31 +1,20 @@
 <?php
    session_start();
-
-   
    if($_SESSION[user]==true)
    {
-     
      header("location: about.html");
    }
    else 
    {
-        $user ="please logint first....";
-        header("location: home.html");
-    
-        
+     $user ="please logint first....";
+     header("location: home.html");  
    }
- 
 ?>
 <script>
     function wellcomeMsg()
-            {
-                var user="<?php echo $user ?>";
-                alert(user);
-            }
-            
-    
-   window.addEventListener('load',wellcomeMsg); 
-   
-  
-           
+    {
+       var user="<?php echo $user ?>";
+       alert(user);
+    }
+   window.addEventListener('load',wellcomeMsg);        
  </script>
